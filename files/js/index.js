fdc4040f57db5cc9ec4a17e43afa3ab46a33879a
userbyte <42632711+userbyte@users.noreply.github.com>
2024-03-23 15:06:22 -0400

    add download botton

---
+++ files/js/index.js
@@ -0,0 +1,17 @@
+function forceDownload(url, fileName){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.responseType = "blob";
+  xhr.onload = function(){
+      var urlCreator = window.URL || window.webkitURL;
+      var imageUrl = urlCreator.createObjectURL(this.response);
+      var tag = document.createElement('a');
+      tag.href = imageUrl;
+      tag.download = fileName;
+      document.body.appendChild(tag);
+      tag.click();
+      document.body.removeChild(tag);
+      link.innerText="Download Image";
+  }
+  xhr.send();
+}

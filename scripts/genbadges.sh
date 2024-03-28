b65882eda8581e721b679afbae73a602843a2bc8
electron271 <66094410+electron271@users.noreply.github.com>
2024-03-28 15:12:05 -0500

    add more badges

--- scripts/genbadges.sh
+++ scripts/genbadges.sh
@@ -8,7 +8,15 @@ BADGES_FILE="files/js/badges.js"
 rm $BADGES_FILE
 touch $BADGES_FILE
 echo "const badges = [" > $BADGES_FILE
-for file in $FOLDER*; do
-  echo "    \"$FOLDER${file##*/}\"," >> $BADGES_FILE
+#for file in $FOLDER*; do
+#  echo "    \"$FOLDER${file##*/}\"," >> $BADGES_FILE
+#done
+
+# go through every file in the badges folder and subfolders
+# note that all files in the badges folder and subfolders are images
+# so we will match ANY file in the badges folder and subfolders
+find $FOLDER -type f | while read file; do
+  echo "    \"$file\"," >> $BADGES_FILE
+  echo "    \"$file\","
 done
 echo "]" >> $BADGES_FILE
